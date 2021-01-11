b93f6b435a28447a31f7ee396bcdb67ffd824130
Luc Döbereiner <luc.doebereiner@gmail.com>
2021-01-11 10:46:38 +0100

    texts modules

---
+++ src/Texts/David.elm
@@ -0,0 +1,7 @@
+module Texts.David exposing (texts)
+
+
+texts : List String
+texts =
+    [ "                                                                                          Artistic research is in its   essence a collective and      collaborative endeavour.                                                                  Artistic research is situated,particular and subjective. It is neither valid nor          objective.                                                                                                              Artistic research does not    produce nor transport         knowledge.                                                                                                                                            Artistic research             stages the conditions for     aesthetic thinking.                                                                                                                                                                                               Formal and material practices are interwoven to each other, nevertheless maintain an      incompressible difference.    This difference is where the  generative potential for      aesthetic research lies.      Artistic research explores andexposes this gap.   "
+    ]
