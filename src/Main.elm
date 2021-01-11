0cdb9132cb623c38d4a8d0d871d96225c2838e82
Luc Döbereiner <luc.doebereiner@gmail.com>
2021-01-11 10:45:38 +0100

    iterations display

--- src/Main.elm
+++ src/Main.elm
@@ -4,18 +4,11 @@ import Element exposing (..)
 import Element.Font as Font
 import Html exposing (pre)
 import Html.Attributes as Attributes
-
-
-gerhard1 =
-    "Artistic research is a radical form of aesthetic practice. It critically engages with the roots of the artist’s doing and thinking.                                                                                                                                                                                                                                                                                               Artistic research does not rival any other form of       research but will use whateverexisting research paradigms   adapted or adaptable to the   aesthetic practice at hand.                                                                                                                                                                                                                                                                                 Artistic research critically  engages with research paradigms, which is its contribution to the general discourse and conceptual develop-development of research."
-        |> String.padRight (40 * 30) ' '
-        |> insertNewlinesEveryN 40
-
-
-david1 =
-    "                                                                                          Artistic research is in its   essence a collective and      collaborative endeavour.                                                                  Artistic research is situated,particular and subjective. It is neither valid nor          objective.                                                                                                              Artistic research does not    produce nor transport         knowledge.                                                                                                                                            Artistic research             stages the conditions for     aesthetic thinking.                                                                                                                                                                                               Formal and material practices are interwoven to each other, nevertheless maintain an      incompressible difference.    This difference is where the  generative potential for      aesthetic research lies.      Artistic research explores andexposes this gap.   "
-        |> String.padRight (40 * 30) ' '
-        |> insertNewlinesEveryN 40
+import List.Extra as L
+import Texts.David as David
+import Texts.Gerhard as Gerhard
+import Texts.Luc as Luc
+import Texts.Ludvig as Ludvig
 
 
 insertNewlinesEveryN : Int -> String -> String
@@ -25,91 +18,17 @@ insertNewlinesEveryN n s =
             ""
 
         str ->
-            String.left n str ++ "\n" ++ insertNewlinesEveryN n (String.dropLeft n str)
+            String.left n str
+                ++ "\n"
+                ++ insertNewlinesEveryN n
+                    (String.dropLeft n str)
 
 
-lucStr1 =
-    "Artistic practice has a conceptual and an aesthetic dimension. Their irreconcilability is a prerequisite.                                                                                                                     Aesthetic thought demonstrates that thought is not tied to language, but that there are unconscious, bodily, felt, material, and practiced forms of thought. Aesthetic thought does not reflect its object at a distance but it is a speculative action that transforms and interacts with material.                                                                                                                                                                                                         There is no method. This is not because artistic practice is based on turmoil, inspiration or whim, but because no abstract scheme of operation can be subtracted from its material entanglement."
+format : String -> String
+format s =
+    s
         |> String.padRight (40 * 30) ' '
         |> insertNewlinesEveryN 40
-
-
-ludvig1 =
-    """Artistic research is a more   
-narrow term than art.         
-                              
-                              
-                              
-                              
-                              
-Artistic research encompasses
-a subset of artistic          
-activities.                   
-                              
-                              
-                              
-                              
-                              
-Artistic research does not    
-require aesthetic adaptation,
-but certain aesthetic         
-approaches are more compatible
-with the superficial traits   
-of traditional modes of       
-research.                     
-                              
-                              
-                              
-                              
-                              
-The idea of artistic research
-is a child of its time, it    
-could not have existed 50     
-years ago, and it might not   
-exist 50 years from now.    """
-
-
-sampleText =
-    """aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-cccccccccccccccccccccccccccccc
-dddddddddddddddddddddddddddddd"""
 
 
 textColumn : String -> Element msg
@@ -117,24 +36,66 @@ textColumn t =
     el [ width shrink, htmlAttribute (Attributes.style "line-height" "2") ] <| html (pre [] [ Html.text t ])
 
 
-ludvigTexts : Element msg
-ludvigTexts =
-    column [ height fill, spaceEvenly ]
-        [ paragraph [] [ text "Artistic research is a more narrow term than art." ]
-        , paragraph [] [ text "Artistic research encompasses a subset of artistic activities." ]
-        , paragraph [] [ text "Artistic research does not require aesthetic adaptation, but certain aesthetic approaches are more compatible with the superficial traits of traditional modes of research." ]
-        , paragraph [] [ text "The idea of artistic research is a child of its time, it could not have existed 50 years ago, and it might not exist 50 years from now." ]
+iteration : String -> String -> String -> String -> Element msg
+iteration david gerhard luc ludvig =
+    row [ centerX, centerY ]
+        [ textColumn (format david)
+        , textColumn " "
+        , textColumn (format gerhard)
+        , textColumn " "
+        , textColumn (format luc)
+        , textColumn " "
+        , textColumn (format ludvig)
         ]
 
 
+repeatLastN : Int -> List String -> List String
+repeatLastN n lst =
+    let
+        lack =
+            n - List.length lst
+    in
+    if lack > 0 then
+        case L.last lst of
+            Just l ->
+                lst ++ List.repeat n l
+
+            Nothing ->
+                lst ++ List.repeat n " "
+
+    else
+        lst
+
+
+padLists : List (List String) -> List (List String)
+padLists lsts =
+    List.map List.length lsts
+        |> List.maximum
+        |> Maybe.map (\n -> List.map (repeatLastN n) lsts)
+        |> Maybe.withDefault lsts
+
+
 main =
-    layout [ width fill, centerY, Font.size 14, Font.family [ Font.monospace ] ] <|
-        row [ centerX, centerY ]
-            [ textColumn david1
-            , textColumn " "
-            , textColumn gerhard1
-            , textColumn " "
-            , textColumn lucStr1
-            , textColumn " "
-            , textColumn ludvig1
-            ]
+    layout [ width fill, centerY, padding 20, Font.size 14, Font.family [ Font.monospace ] ] <|
+        column [ width fill, spacingXY 0 30 ] <|
+            List.map
+                (\l ->
+                    case l of
+                        [ d, g, luc, ludvig ] ->
+                            iteration d g luc ludvig
+
+                        _ ->
+                            let
+                                _ =
+                                    Debug.log "l" l
+                            in
+                            text "Problem"
+                )
+                (padLists
+                    [ David.texts
+                    , Gerhard.texts
+                    , Luc.texts
+                    , Ludvig.texts
+                    ]
+                    |> L.transpose
+                )
